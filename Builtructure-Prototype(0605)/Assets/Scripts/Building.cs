52da8d8b2f3473284275296199ba507383eed6f3
박범수 <pbs1219@gmail.com>
2022-06-06 15:56:26 +0900

    Builtructure-Prototype
    정상적인 협업 및 변경사항 관리를 위해 기존파일 삭제 후, gitignore 적용시켜 관리할 파일만 다시 업로드

---
+++ Builtructure-Prototype(0605)/Assets/Scripts/Building.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class Building : MonoBehaviour
+{
+    public string buildingName;
+    public int populationNeed;
+    public int energyNeed;
+    public int woodNeed;
+    public int stoneNeed;
+    public int ironNeed;
+    
+    public int populationProduce;
+    public int energyProduce;
+    public int woodNeedProduce;
+    public int stoneProduce;
+    public int ironProduce;
+    
+    public bool Placed { get; private set; }
+    public BoundsInt area;
+    
+
+    #region Build Methods
+
+    public bool CanBePlaced()
+    {
+        Vector3Int positionInt = GridBuildingSystem.current.gridLayout.LocalToCell(transform.position);
+        BoundsInt areaTemp = area;
+        areaTemp.position = positionInt;
+
+        if (GridBuildingSystem.current.CanTakearea(areaTemp))
+        {
+            if (GridBuildingSystem.current.CheckEnoughResource(this))
+            {
+                return true;
+            }
+            return false;
+        }
+        return false;
+    }
+
+    public void Place()
+    {
+        Vector3Int positionInt = GridBuildingSystem.current.gridLayout.LocalToCell(transform.position);
+        BoundsInt areaTemp = area;
+        areaTemp.position = positionInt;
+        Placed = true;
+        GridBuildingSystem.current.TakeArea(areaTemp);
+        
+    }
+
+    #endregion
+}
---
+++ Builtructure-Prototype(0605)/Assets/Scripts/Player.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public String userName;
+    public String gameDate;
+
+    public float support;
+    public float efficiency;
+    public float order;
+    public float satisfaction;
+    
+    public int population;
+    public int energy;
+    public int wood;
+    public int stone;
+    public int iron;
+
+    void Start()
+    {
+
+    }
+}
