d98b8dfd1d5c44a388e6b77d73b9cc7b20afe6c7
박범수 <pbs1219@gmail.com>
2022-08-07 19:39:02 +0900

    0807-tunnelvision

---
+++ Assets/GameManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class GameManager : MonoBehaviour
+{
+
+    public Tilemap DarkMap;
+
+    public Tilemap Ground;
+
+    public Tile DarkTile;
+    // Start is called before the first frame update
+    void Start()
+    {
+        DarkMap.origin = Ground.origin;
+        DarkMap.size = Ground.size;
+
+        foreach (Vector3Int p in DarkMap.cellBounds.allPositionsWithin)
+        {
+            DarkMap.SetTile(p, DarkTile);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/SMask.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SMask : MonoBehaviour
+{
+    [Range(0.05f,0.2f)] public float flickTime;
+    [Range(0.02f,0.09f)] public float addSize;
+
+    float timer = 0;
+    private bool bigger = true;
+
+    public Transform target;
+    
+    
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (timer > flickTime)
+        {
+            if (bigger)
+            {
+                transform.localScale = new Vector3(transform.localScale.x + addSize, transform.localScale.y + addSize,
+                    transform.localScale.z);
+            }
+            else
+            {
+                transform.localScale = new Vector3(transform.localScale.x - addSize, transform.localScale.y - addSize,
+                    transform.localScale.z);
+            }
+
+            timer = 0;
+            bigger = !bigger;
+
+        }
+
+        transform.position = Vector3.MoveTowards(transform.position, target.position, 20 * Time.deltaTime);
+    }
+}
