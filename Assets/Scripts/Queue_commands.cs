9509271c7ef928bb181725199be24768dabf4580
hoyong1251 <64102541+hoyong1251@users.noreply.github.com>
2022-08-13 00:07:23 +0900

    0813
    <Queue>
    +move Button func

--- Assets/Scripts/Queue_commands.cs
+++ Assets/Scripts/Queue_commands.cs
@@ -6,12 +6,14 @@ public class Queue_commands : MonoBehaviour
 {
     public GameObject[] prefabs;
     public GameObject Parent;
+    public GameObject car;
     private Vector2 create_point;
     private RectTransform rect_obj , Parent_rect;
-    
-    private int count = 0;
+
+    private static int count = 0;
     private int size = 12; // width 에 따라 변경?
-    
+    private int i = 0;
+    private int[] queue; // 0 :up , 1: right , 2: left , 3: down
 
     bool sizeCheck()
     {
@@ -23,7 +25,10 @@ public class Queue_commands : MonoBehaviour
             return false;
         }
     }
-
+    void Awake()
+    {
+        queue = new int[size];
+    }
 
     public void create_upblocks()
     {
@@ -35,6 +40,7 @@ public class Queue_commands : MonoBehaviour
             float C_width = rect_obj.rect.width;
 
             rect_obj.anchoredPosition = new Vector2(P_width/2 - C_width/2 - count*81f, 0);
+            queue[count] = 0;
             count++;
         }
         else return;
@@ -49,6 +55,7 @@ public class Queue_commands : MonoBehaviour
             float C_width = rect_obj.rect.width;
 
             rect_obj.anchoredPosition = new Vector2(P_width/2 - C_width/2 - count*81f, 0);
+            queue[count] = 1;
             count++;
         }
         else return;
@@ -63,6 +70,7 @@ public class Queue_commands : MonoBehaviour
             float C_width = rect_obj.rect.width;
 
             rect_obj.anchoredPosition = new Vector2(P_width/2 - C_width/2 - count*81f, 0);
+            queue[count] = 2;
             count++;
         }
         else return;
@@ -77,8 +85,46 @@ public class Queue_commands : MonoBehaviour
             float C_width = rect_obj.rect.width;
 
             rect_obj.anchoredPosition = new Vector2(P_width/2 - C_width/2 - count*81f, 0);
+            queue[count] = 3;
             count++;
         }
         else return;
+    }
+
+    public void move_car()
+    {
+        
+        if(i <= count-1)
+        {
+            switch (queue[i])
+            {
+                case 0:
+                    car.GetComponent<Car_Queue>().carMoveUp();
+                    break;
+                case 1:
+                    car.GetComponent<Car_Queue>().carMoveRight();
+                    break;
+                case 2:
+                    car.GetComponent<Car_Queue>().carMoveLeft();
+                    break;
+                case 3:
+                    car.GetComponent<Car_Queue>().carMoveDown();
+                    break;
+            }
+            Destroy(transform.GetChild(0).gameObject);
+            i++;
+            if (i == count)
+            {
+                CancelInvoke("move_car");
+                Debug.Log("이동 종료");
+                return;
+            }
+            
+            Invoke("move_car", 1.34f);
+        }
+        else
+        {
+            
+        }
     }
 }
