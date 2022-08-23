45ac62bebe929562e650b6d95a80b37c1b34315c
hoyong1251 <64102541+hoyong1251@users.noreply.github.com>
2022-08-23 14:52:06 +0900

    0823
    <Graph>
    1)indicator 성공
    2) 방향벡터에 따른 타일 변경
    알고리즘 연구 진행

---
+++ Assets/Scripts/dragIndicator.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class dragIndicator : MonoBehaviour
+{
+
+    private bool button_clicked = false;
+
+
+    public GridLayout gridLayout;
+    public Tilemap MainTilemap;
+    public Tilemap TempTilemap;
+    private Vector3Int prevPos;
+
+    void Start()
+    {
+    }
+    void Update()
+    {
+        if (button_clicked)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                Vector2 touchPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                Vector3Int cellPos = gridLayout.LocalToCell(touchPos);
+                Debug.Log(cellPos);
+                prevPos = cellPos;
+            }
+            if (Input.GetMouseButton(0))
+            {
+                Vector2 touchPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                Vector3Int cellPos = gridLayout.LocalToCell(touchPos);
+                if(prevPos != cellPos)
+                {
+                    Debug.Log("moved to" +cellPos);
+                }
+            }
+            if (Input.GetMouseButtonUp(0))
+            {
+
+            }
+        }
+    }
+
+    public void btn_click()
+    {
+        this.button_clicked = true;
+         // 도로 배치 후 false 로 바꿔줘야함
+    }
+
+}
