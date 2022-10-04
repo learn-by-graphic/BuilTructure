d4a10452db27983044679f6aa0b23643f513d402
doluu59 <73704383+doluu59@users.noreply.github.com>
2022-10-04 16:28:44 +0900

    1004 sort frame 단위 움직임 실험
    좌표 값이 너무 작아 이동 모션 주기 어려움.
    
    ++ 스택 스테이지 무브 버튼에 클릭 이벤트가 사라졌던데 수정해야될듯?

---
+++ Assets/Scripts/SortBlockMove.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SortBlockMove : MonoBehaviour
+{
+    //Block Moving Tool
+    int moveType;
+    bool deadlock = false;
+
+    Vector3 startpos;
+    Vector3 targetpos;
+
+    public float m_Speed = 500.0f;  //450
+    public float m_HeightArc = -150.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //block Moving
+        deadlock = true;
+        switch (moveType)
+        {
+            case 1:
+                {
+                    m_HeightArc = -150.0f;
+                    break;
+                }
+            case 2:
+                {
+                    m_HeightArc = 150.0f;
+                    break;
+                }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (deadlock)
+        {
+            Debug.Log("update");
+            float x0 = startpos.x;      //출발 x
+            float x1 = targetpos.x;     //도착 x
+            float distance = x1 - x0;   //x좌표 거리 차이
+            float nextX = Mathf.MoveTowards(transform.position.x, x1, m_Speed * Time.deltaTime);
+            float baseY = Mathf.Lerp(startpos.y, targetpos.y, (nextX - x0) / distance);
+            float arc = m_HeightArc * (nextX - x0) * (nextX - x1) / (-0.25f * distance * distance);
+            Vector3 nextPosition = new Vector3(nextX, baseY + arc, transform.position.z);
+
+            //transform.rotation = LookAt2D(nextPosition - transform.position);     //회전
+            transform.position = nextPosition;
+
+            if (nextPosition == targetpos)
+            {
+                Debug.Log("arrive");
+                Arrived();
+                return;
+            }
+        }
+    }
+
+    public void letsMove(int index, Vector3 start, Vector3 dst)
+    {
+        moveType = index;
+        startpos = start;
+        targetpos = dst;
+        deadlock = true;
+    }
+
+    void Arrived()
+    {
+        deadlock = false;
+    }
+}
+
---
+++ Assets/Scripts/SortScript.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SortScript : MonoBehaviour
+{
+    public GameObject BlockToMove;  //현재 블록, 자리를 옮겨 줄
+    public GameObject BlockToBeMoved;   //위치가 바뀌어질 대상 블록
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+
+
+    public void SwitchLocation()
+    {
+        //when block 1,2 is Clicked OR switch button 'YES' is Clicked
+        //위치 바꿔줄 블록을 선택하기 위한 사용자 동작
+
+        //Block GameObjects that have to be switched
+        //위치 바꿀 블록 선정
+        BlockToMove = GameObject.Find("box1");
+        BlockToBeMoved = GameObject.Find("box2");
+
+        BlockToMove.AddComponent<SortBlockMove>();
+        BlockToMove.GetComponent<SortBlockMove>().letsMove(1, BlockToMove.transform.position, BlockToBeMoved.transform.position);
+
+        BlockToBeMoved.AddComponent<SortBlockMove>();
+        BlockToBeMoved.GetComponent<SortBlockMove>().letsMove(1, BlockToBeMoved.transform.position, BlockToMove.transform.position);
+
+
+
+    }
+}
+
+
+
+
