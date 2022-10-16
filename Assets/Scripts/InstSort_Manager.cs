6e5ac24341c209e6b57a4fddc89cc9a8a9f70551
hoyong1251 <64102541+hoyong1251@users.noreply.github.com>
2022-10-16 18:44:00 +0900

    1016
    <sort_insertion>
    범용성 설계 완료
    ani와 deltatime 시간차 연결
    
    **In Progress : sort_insertion complete

---
+++ Assets/Scripts/InstSort_Manager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstSort_Manager : MonoBehaviour
+{
+    public bool move_on = false;
+    public float move_speed = 2f;
+    public GameObject[] boxes;
+    private Vector3[] target_positions;
+    private string box_string = "";
+    private int phase_count;
+    // Start is called before the first frame update
+    void Start()
+    {
+        float x_pos = -6f;
+        boxes = new GameObject[8];
+        target_positions = new Vector3[8];
+        for (int i = 1; i < 9; i++)
+        {
+            box_string = "box" + "" + i;
+            target_positions[i-1] = new Vector3(x_pos + (1.5f * (i-1)), -2, 0);
+            boxes[i - 1] = GameObject.Find(box_string);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (move_on)
+        {
+            vector_move(boxes[6], target_positions[1]);
+        }
+    }
+
+    public void animation_move()
+    {
+        move_on = true;
+        boxes[4].GetComponent<Animation>().Play();
+    }
+
+    public void vector_move(GameObject box, Vector3 tar_pos)
+    {
+        box.transform.position = Vector3.MoveTowards(box.transform.position , tar_pos, move_speed * Time.deltaTime);
+        if (box.transform.position == tar_pos)
+            move_on = false;
+    }
+}
